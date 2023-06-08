6bdee7d57bad5b1c9d9281d5ad5b980be1e4e384
itliangwj <liangwenjie@163.com>
2023-06-08 21:31:55 +0800

    【my-spring-framework】模块增加自定义JRequestParam注解

---
+++ my-spring-framework/src/main/java/com/jet/mvcframework/annotation/JRequestParam.java
@@ -0,0 +1,18 @@
+package com.jet.mvcframework.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * @author Jet
+ * @version 1.0
+ * @description: TODO
+ * @date 2023/6/4 8:48
+ */
+//@Target 决定注解的适用范围
+@Target({ElementType.PARAMETER})
+//@Retention 决定注解的生命周期，Runtime表示整个程序运行期间皆有效
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface JRequestParam {
+    String value() default "";
+}
